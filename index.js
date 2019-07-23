cd2f13ff7473e3103486896f5da729894ac1908d
bruceallen <boacinema@gmail.com>
2019-07-23 01:52:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,24 @@ express()
   .get('/dbcreate', async (req, res) => {
     try {
       const client = await pool.connect()
+      const oldcount = await client.query('SELECT * FROM test_table');
+      console.error("ERRORBOY " + oldcount[1].count);     
       const result1 = await client.query("DROP TABLE test_table");
+      const result2 = await client.query("CREATE TABLE test_table(id SERIAL PRIMARY KEY, count INT)");
+      const results = { 'results': (result) ? result2.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
+  .get('/dbmore', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const oldcount = await client.query('SELECT * FROM test_table');
+      console.error("ERRORBOY " + oldcount[1].count);     
       const result2 = await client.query("CREATE TABLE test_table(id SERIAL PRIMARY KEY, count INT)");
       const results = { 'results': (result) ? result2.rows : null};
       res.render('pages/db', results );
@@ -46,6 +63,8 @@ express()
   .get('/dbinsert', async (req, res) => {
     try {
       const client = await pool.connect()
+      const oldcount = await client.query('SELECT * FROM test_table');
+      
       const result = await client.query("INSERT INTO test_table(id, count) VALUES(3, 9)");
       const results = { 'results': (result) ? result.rows : null};
       res.render('pages/db', results );
