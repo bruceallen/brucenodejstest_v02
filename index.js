142660a2267b5f7dc4d5f79874e5179261ed518a
bruceallen <boacinema@gmail.com>
2019-07-23 23:44:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -445,10 +445,9 @@ express()
           
           res.writeHead(200, {'Content-Type': 'text/html'});
           res.write('<center>');
-          res.write(randomStatus + '<br> />');
-          //          res.write(query1 + '<br />');
+          res.write(randomStatus + '<br />');
+//          res.write(query1 + '<br />');
 //          res.write(query2 + '<br />');
-          res.write(randomStatus + '<br> />');
 //          res.write('<b>Hey there!</b><br /><br />This is the default response. You are visitor #: ' + newcount);
           res.write('<br><br><br><button onclick="refreshPage()">Generate New PackCon Status</button><br><br></center>');
           
