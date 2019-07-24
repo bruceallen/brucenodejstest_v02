699c064d615d06437f4d78da354baaac9bc28cfb
bruceallen <boacinema@gmail.com>
2019-07-24 02:20:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -328,21 +328,6 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-//  .get('/', (req, res) => res.render('pages/index'))
-
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM phrase_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-
   .get('/showdatabase', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -433,3 +418,20 @@ express()
   })
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// UNUSED 
+/*  .get('/', (req, res) => res.render('pages/index'))
+
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM phrase_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+*/
